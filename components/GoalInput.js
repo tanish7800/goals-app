1b91c476d8ccc65453e8045e3acefa98f497d4f2
tanish7800 <studiomac-149@studioMac-149s-iMac.local>
2022-03-03 11:32:44 +0530

    first commit

---
+++ components/GoalInput.js
@@ -0,0 +1,57 @@
+import React,{ useState } from 'react';
+import { View , Text , Button , TextInput , StyleSheet , Modal } from 'react-native';
+
+
+const GoalInput = props => {
+    const [enteredGoal, setEnteredGoal] = useState(''); 
+
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText);
+    };
+
+    const addGoalHandler = () => {
+        props.onAddGoal(enteredGoal);
+        setEnteredGoal(' ');
+    };
+
+    return(
+        <Modal visible={props.visible} animationType='slide' >
+            <View style={styles.inputContainer}>
+                <TextInput 
+                    textAlign='center'
+                    placeholder='Add Stuff!'
+                    style={styles.textInput} 
+                    onChangeText={goalInputHandler} 
+                    value={enteredGoal} />
+                <View style={styles.buttonContainer} > 
+                    <View style = {styles.button}><Button title="ADD" onPress={addGoalHandler} /></View>
+                    <View style = {styles.button}><Button title="CANCEL"  color="red" onPress={props.onCancel}/></View>
+                </View>
+            </View>
+        </Modal>);
+};
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex:1
+      },
+      textInput: {
+        borderColor: 'black',
+        borderWidth: 1,
+        width: '75%',
+        height:35,
+        marginBottom:10
+      },
+      buttonContainer:{
+          flexDirection:'row',
+          justifyContent:'space-around',
+          width:'60%'
+      },
+      button:{
+          width:'40%'
+      }
+})
+
+export default GoalInput;
---
+++ components/GoalItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View , Text , StyleSheet , TouchableOpacity } from 'react-native';
+
+const GoalItem = props => {
+    return (<TouchableOpacity activeOpacity={0.8} onPress={props.onDelete.bind(this, props.id)} >
+                <View style={styles.listItem} >
+                <Text >{props.title}</Text>
+                </View>
+            </TouchableOpacity>);
+};
+
+const styles = StyleSheet.create({
+    listItem: {
+        padding: 10,
+        margin: 5,
+        backgroundColor: '#ccc',
+        borderColor: 'black',
+        borderWidth: 1
+      }
+});
+
+export default GoalItem;
